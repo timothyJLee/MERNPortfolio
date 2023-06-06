de279617258d78b1e2821c5f67c60dea9d205215
Timothy Lee <timothy.j.lee.cs@gmail.com>
2023-06-06 13:24:24 -0400

    model schema

---
+++ server/model/task.model.ts
@@ -0,0 +1,23 @@
+import { model, Schema, Model, Document } from "mongoose";
+
+export interface ITask extends Document {
+  name: string;
+  description: string;
+  finished: boolean;
+  createDate: string;
+  updatedDate: string;
+  createdBy: string;
+  updatedBy: string;
+}
+
+const TaskSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  finished: { type: Boolean, required: true },
+  createDate: { type: Date, default: Date.now },
+  updatedDate: { type: Date, default: Date.now },
+  createdBy: { type: String, required: false },
+  updatedBy: { type: String, required: false },
+});
+
+export const TaskModel: Model<ITask> = model<ITask>("todos", TaskSchema);
